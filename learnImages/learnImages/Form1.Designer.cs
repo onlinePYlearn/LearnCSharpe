d9761cb201a9c60800a85f3cdcb1769122d7cd68
chenpy <chenpy@solab.me.ntu.edu.tw>
2018-10-21 15:07:34 +0800

    show image and save it

--- learnImages/learnImages/Form1.Designer.cs
+++ learnImages/learnImages/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(27, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(959, 528);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(899, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 600);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- learnImages/learnImages/Form1.cs
+++ learnImages/learnImages/Form1.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace learnImages
 {
     public partial class Form1 : Form
     {
+        Bitmap image1;
         public Form1()
         {
             InitializeComponent();
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Retrieve the image.
+                image1 = new Bitmap(@"E:\data\CSharpe\LearnCSharpe\learnImages\"+
+                    @"learnImages\sample\violet.jpg", true);
+
+                int x, y;
+
+                // Loop throught the images pixels to reset color.
+                for (x=0; x<image1.Width; x++)
+                {
+                    for (y=0; y<image1.Height; y++)
+                    {
+                        Color pixelColor = image1.GetPixel(x, y);
+                        Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                        image1.SetPixel(x, y, newColor);
+                    }
+                }
+
+                // Set the PictureBox to display the image.
+                pictureBox1.Image = image1;
+
+                // Display the pixel format in Label1.
+                label1.Text = "Pixel format: " + image1.PixelFormat.ToString();
+
+            }
+            catch(ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                    "Check the path to the image file.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (image1 != null)
+            {
+                string fname = @"E:\data\CSharpe\LearnCSharpe\learnImages\" +
+                    @"learnImages\sample\violet_result.jpg";
+                image1.Save(fname, ImageFormat.Jpeg);
+            }
         }
     }
 }
